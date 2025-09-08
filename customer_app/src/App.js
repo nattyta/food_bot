c515683af2f91b5ba232a1eff6e98623fe6d5e29
nattyta <128259155+nattyta@users.noreply.github.com>
2025-09-08 11:18:56 +0300

    payment redirect page

--- customer_app/src/App.js
+++ customer_app/src/App.js
@@ -253,7 +253,6 @@ function App() {
           <Route path="/" element={<HomePage cart={cart} setCart={setCart} user={auth?.user}  telegramInitData={telegramInitData} />} />
           <Route path="/detail" element={<Detail cart={cart} setCart={setCart} />} />
           <Route path="/order-history" element={<OrderHistory />} />
-          
           <Route path="/CartPage" element={
             <CartPage 
               cart={cart} 
@@ -273,7 +272,8 @@ function App() {
             } />
           <Route path="/payment" element={<PaymentPage />} />
 
-          
+          <Route path="/history" element={<HistoryPage telegramInitData={telegramInitData} />} /> 
+          <Route path="/payment-success" element={<PaymentStatusPage />} />
         </Routes>
 
        
